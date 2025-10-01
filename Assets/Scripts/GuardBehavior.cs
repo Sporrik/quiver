d52dc0863eda49988e41fc1a1adf0ae582bb5c1c
EatenMelon <ignace.dewulf2@gmail.com>
2025-10-01 21:15:00 +0200

    Guard AI & ground fix
    - made it so the guards can't see through walls
    - fix the guards falling through the floor

--- Assets/Scripts/GuardBehavior.cs
+++ Assets/Scripts/GuardBehavior.cs
@@ -5,8 +5,10 @@ public class GuardBehavior : MonoBehaviour
 {
     [SerializeField] private Transform _player;
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private GameObject _eyes;
+
     private NavMeshAgent _agent;
-    [SerializeField]  private float _sightRange, _attackRange;
+    [SerializeField] private float _sightRange, _attackRange;
     private bool _isPlayerInSightRange, _isPlayerInAttackRange;
 
     private void Start()
@@ -17,18 +19,34 @@ public class GuardBehavior : MonoBehaviour
 
     private void Update()
     {
-        _isPlayerInSightRange = Physics.CheckSphere(transform.position, _sightRange, playerLayer);
-        _isPlayerInAttackRange = Physics.CheckSphere(transform.position, _attackRange, playerLayer);
-
-        if (_isPlayerInSightRange && !_isPlayerInAttackRange)
-        {
-            ChasePlayer();
-        }
-        else
-        {
-            _agent.SetDestination(transform.position);
-        } 
+        PlayerDetection();
     }
 
     private void ChasePlayer() => _agent.SetDestination(_player.position);
+
+    static readonly string[] SIGHT_BLOCK_MASK = { "Ground", "StaticLevel" };
+    void PlayerDetection()
+    {
+        _isPlayerInSightRange = Physics.CheckSphere(transform.position, _sightRange, playerLayer);
+        _isPlayerInAttackRange = Physics.CheckSphere(transform.position, _attackRange, playerLayer);
+
+        // check if player is in range
+        if (_isPlayerInSightRange && !_isPlayerInAttackRange)
+        {
+            if (_eyes == null) return;
+
+            // check if something is obstruction the guards line of sight
+            Ray ray = new Ray(_eyes.transform.position, (_player.transform.position - _eyes.transform.position).normalized);
+
+            if (!Physics.Raycast(ray, _sightRange, LayerMask.GetMask(SIGHT_BLOCK_MASK)))
+            {
+                ChasePlayer();
+            }
+        }
+        else
+        {
+            // there should be something extra here later on, so it can feel more natural (maybe a delay)
+            _agent.SetDestination(transform.position);
+        }
+    }
 }
