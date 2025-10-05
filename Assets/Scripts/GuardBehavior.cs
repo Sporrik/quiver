3b166bb328b494924c80010ece97d44e6fc3ba24
EatenMelon <ignace.dewulf2@gmail.com>
2025-10-05 16:27:28 +0200

    Updated guard AI
    - guards can now only see in front of them within a adjustable angle
    
    - guards can now follow a path, which we can create with empty game objects that we can assign in a list in the guard game object

--- Assets/Scripts/GuardBehavior.cs
+++ Assets/Scripts/GuardBehavior.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,9 +9,13 @@ public class GuardBehavior : MonoBehaviour
     [SerializeField] private Transform _player;
     [SerializeField] private LayerMask playerLayer;
     [SerializeField] private GameObject _eyes;
+    [SerializeField] private bool _seesPlayer;
+
+    [SerializeField] private List<GameObject> _path;
+    private int _currentPathPoint;
 
     private NavMeshAgent _agent;
-    [SerializeField] private float _sightRange, _attackRange;
+    [SerializeField] private float _sightRange, _sightAngle, _attackRange;
     private bool _isPlayerInSightRange, _isPlayerInAttackRange;
 
     private void Start()
@@ -20,12 +27,13 @@ public class GuardBehavior : MonoBehaviour
     private void Update()
     {
         PlayerDetection();
+        FollowPath();
     }
 
     private void ChasePlayer() => _agent.SetDestination(_player.position);
 
     static readonly string[] SIGHT_BLOCK_MASK = { "Ground", "StaticLevel" };
-    void PlayerDetection()
+    private void PlayerDetection()
     {
         _isPlayerInSightRange = Physics.CheckSphere(transform.position, _sightRange, playerLayer);
         _isPlayerInAttackRange = Physics.CheckSphere(transform.position, _attackRange, playerLayer);
@@ -35,18 +43,45 @@ public class GuardBehavior : MonoBehaviour
         {
             if (_eyes == null) return;
 
-            // check if something is obstruction the guards line of sight
-            Ray ray = new Ray(_eyes.transform.position, (_player.transform.position - _eyes.transform.position).normalized);
+            Vector3 directionToPlayer = (_player.transform.position - _eyes.transform.position).normalized;
+            float angleToPlayer = Vector3.Angle(_eyes.transform.forward, directionToPlayer);
 
-            if (!Physics.Raycast(ray, _sightRange, LayerMask.GetMask(SIGHT_BLOCK_MASK)))
+            if (angleToPlayer <= _sightAngle / 2f)
             {
-                ChasePlayer();
+                Ray ray = new Ray(_eyes.transform.position, directionToPlayer);
+
+                if (!Physics.Raycast(ray, _sightRange, LayerMask.GetMask(SIGHT_BLOCK_MASK)))
+                {
+                    _seesPlayer = true;
+                    ChasePlayer();
+                }
+                else
+                {
+                    _seesPlayer = false;
+                }
             }
+            else
+            {
+                _seesPlayer = false;
+            }
+
         }
-        else
+    }
+
+    private void FollowPath()
+    {
+        if (_seesPlayer) return;
+
+        if (_path[0] == null) return;
+
+        if((_path[_currentPathPoint].transform.position - transform.position).magnitude <= _attackRange)
         {
-            // there should be something extra here later on, so it can feel more natural (maybe a delay)
-            _agent.SetDestination(transform.position);
+            ++_currentPathPoint;
+
+            if (_currentPathPoint > _path.Count - 1) _currentPathPoint = 0;
         }
+
+        _agent.SetDestination(_path[_currentPathPoint].transform.position);
+
     }
 }
