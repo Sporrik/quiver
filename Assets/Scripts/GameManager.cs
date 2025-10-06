836a427cb612406e3bb050ccb37ed40874e0d9bc
Sporrik <ahmadyolo@gmail.com>
2025-10-06 14:49:54 +0200

    Added Manager scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public enum GameState { };
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private GameState _gameState;
+    public GameState gameState { get { return _gameState; } }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
