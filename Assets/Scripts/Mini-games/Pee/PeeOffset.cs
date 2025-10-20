c1ce525202be15d729b693835e07f35f059380fc
timotheseverijn <timothe.sev@gmail.com>
2025-10-20 17:28:50 +0200

    Functional Pee Minigame

---
+++ Assets/Scripts/Mini-games/Pee/PeeOffset.cs
@@ -0,0 +1,30 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PeeOffset : MonoBehaviour
+{
+    public Transform PeeOrigin;
+    public Transform PeeTarget;
+    public ParticleSystem piss;
+    public Transform ParticleParent;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = 
+            new Vector3
+            (
+                transform.position.x + transform.rotation.x,
+                gameObject.transform.position.y,
+                PeeTarget.transform.transform.position.z
+            );
+       
+        piss.startSpeed = (PeeOrigin.transform.position - gameObject.transform.position).magnitude * 3.25f;
+
+    }
+}
