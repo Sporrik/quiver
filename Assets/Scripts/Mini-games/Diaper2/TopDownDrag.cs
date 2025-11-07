2241a0b9c1c196cad27506d9de26869ccddac669
timotheseverijn <timothe.sev@gmail.com>
2025-11-07 19:12:42 +0100

    Progress on diaper minigame

---
+++ Assets/Scripts/Mini-games/Diaper2/TopDownDrag.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class TopDownDrag : MonoBehaviour
+{
+    private Rigidbody rb;
+    public bool IsDragging = false;
+
+    public GameObject CurrentlyDraggedObject;
+
+    public float DragPlaneY = 2f; // Hover height
+
+    private Camera mainCamera;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
+    }
+
+    void OnMouseDown()
+    {
+        IsDragging = true;
+        rb.useGravity = false;
+        rb.linearVelocity = Vector3.zero;
+
+        CurrentlyDraggedObject = gameObject;
+    }
+
+    public void OnMouseUp()
+    {
+        IsDragging = false;
+        rb.useGravity = true;
+
+        if (CurrentlyDraggedObject == gameObject)
+            CurrentlyDraggedObject = null;
+
+    }
+
+    void Update()
+    {
+        if (IsDragging)
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            Plane dragPlane = new Plane(Vector3.up, new Vector3(0f, DragPlaneY, 0f));
+            if (dragPlane.Raycast(ray, out float enter))
+            {
+                Vector3 hitPoint = ray.GetPoint(enter);
+
+                Vector3 targetPosition = new Vector3(hitPoint.x, DragPlaneY, hitPoint.z);
+                rb.MovePosition(Vector3.Lerp(transform.position, targetPosition, 0.4f));
+            }
+        }
+    }
+}
