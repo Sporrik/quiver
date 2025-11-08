e3282a833d32cc13c5cd1b8a3fc3f33e352b3d2d
FreddyFuckBoy <finrovers@gmail.com>
2025-11-08 22:33:19 +0100

    Interaction system
    Tried workign on the interaction system and made a door interaction object, interaction interface script and door interaction script

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -7,11 +7,35 @@ public class PlayerInteraction : MonoBehaviour
     [SerializeField] private float _attackRange;
     [SerializeField] private LayerMask _enemyLayer;
 
+    [SerializeField] private Transform _interactionPoint;
+    [SerializeField] private float _interactionRange = 2f;
+    [SerializeField] private LayerMask _interactionLayer;
+
+    private IInteractable _currentInteractable;
+
     public void Interact(InputAction.CallbackContext context)
     {
         if (!context.started) return;
 
         //interaction stuff here
+        if (_currentInteractable != null)
+        {
+            _currentInteractable.Interact();
+        }
+        else
+        {
+            Collider[] hits = Physics.OverlapSphere(_interactionPoint.position, _interactionRange, _interactionLayer);
+            foreach (Collider hit in hits)
+            {
+                IInteractable interactable = hit.GetComponent<IInteractable>();
+                if(interactable != null)
+                {
+                    _currentInteractable = interactable;
+                    _currentInteractable.Interact();
+                    break;
+                }
+            }
+        }
     }
 
      public void Takedown(InputAction.CallbackContext context)
@@ -28,6 +52,28 @@ public class PlayerInteraction : MonoBehaviour
                 Destroy(hitGO);
                 break;
             }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //This line creates a mask and checks whether that layer is included in the interactionLayer Mask
+        //If so, the object is on the allowed interactable layer
+        if (((1 << other.gameObject.layer) & _interactionLayer) != 0)
+        {
+            IInteractable interactable = other.GetComponent<IInteractable>();
+            if (interactable != null)
+            {
+                _currentInteractable = interactable;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(_currentInteractable != null && other.GetComponent<IInteractable>() == _currentInteractable)
+        {
+            _currentInteractable = null;
         }
     }
 }
