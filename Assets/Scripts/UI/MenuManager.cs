5a931257257807a67a1769540f56ad1b69e7a50f
Maksel636 <axeldruyts@gmail.com>
2025-11-10 14:00:34 +0100

    menumanager + ui layout

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -1,20 +1,36 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
 
+    [SerializeField] private GameObject _twitchScreen;
     [SerializeField] private GameObject _controlScreen;
+
+
     public void OnSinglePlayer()
     {
+
+        // set it for singleplayer
+        SceneManager.LoadScene("LevelOne");
+
         Debug.Log("Single");
     }
 
     public void OnTwitch()
     {
-        Debug.Log("twitch");
+        _twitchScreen.SetActive(true);
 
+    }
+
+    public void OnPlayWithTwitch()
+    {
+        // check if it is autorized;
+        // set it for multiplayer
+        SceneManager.LoadScene("LevelOne");
+        print("LOAD GAME");
     }
 
     public void OnControls()
@@ -30,6 +46,7 @@ public class MenuManager : MonoBehaviour
     public void OnBacktoMainMenu()
     {
         _controlScreen.SetActive(false);
+        _twitchScreen.SetActive(false);
 
     }
 
