1bd9c115f170dab223ce9fc7125b04a163c87b4b
Maksel636 <axeldruyts@gmail.com>
2025-11-10 12:01:41 +0100

    UIMenu

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,34 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public void OnSinglePlayer()
+    {
+        Debug.Log("Single");
+    }
+
+    public void OnTwitch()
+    {
+        Debug.Log("twitch");
+
+    }
+
+    public void OnControls()
+    {
+
+    }
+
+    public void OnHowToPlay()
+    {
+
+    }
+
+
+
+
+
+
+
+}
