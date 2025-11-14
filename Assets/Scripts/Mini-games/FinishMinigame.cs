034b1604d550950657fed9ee50d2489d0fdb079b
Ignace Dewulf <ignace.dewulf2@gmail.com>
2025-11-14 17:09:25 +0100

    rewinded level one after norrs commit

--- Assets/Scripts/Mini-games/FinishMinigame.cs
+++ Assets/Scripts/Mini-games/FinishMinigame.cs
@@ -5,7 +5,7 @@ public class FinishMinigame : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        MinigameScreen screen = GetComponent<MinigameScreen>();
     }
 
     // Update is called once per frame
