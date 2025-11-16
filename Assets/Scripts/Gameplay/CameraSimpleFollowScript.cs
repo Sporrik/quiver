e852e5e1be1ce152d2f2bc94c6b688ffe68f13d3
Sporrik <ahmadyolo@gmail.com>
2025-11-16 15:20:09 +0100

    bar fix pt 1

--- Assets/Scripts/Gameplay/CameraSimpleFollowScript.cs
+++ Assets/Scripts/Gameplay/CameraSimpleFollowScript.cs
@@ -2,16 +2,9 @@ using UnityEngine;
 
 public class CameraSimpleFollowScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
     [SerializeField] private GameObject _player;
     [SerializeField] private Vector3 _distance;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = _player.transform.position + _distance;
