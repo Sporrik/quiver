c0c4052259763a9e8c36a84fe65ba7bff1b76ad1
Maksel636 <axeldruyts@gmail.com>
2025-11-24 15:25:00 +0100

    vieuwercount

--- Assets/Scripts/UI/UIMeter.cs
+++ Assets/Scripts/UI/UIMeter.cs
@@ -17,7 +17,8 @@ namespace UI
 
         [Header("Animation")]
         [SerializeField] private float _delayAddSeconds = 2f;
-        private IEnumerator coroutine;
+
+        private TwitchGameManager _twitchGameManager;
 
 
         private UIScriptableObject _uiData;
@@ -83,8 +84,8 @@ namespace UI
 
         private void OnValue(float value01_100)
         {
-
-
+            value01_100 /= _twitchGameManager.GetViewerCount();
+            Debug.Log(value01_100);
 
             if (_image == null) return;
 
@@ -102,7 +103,7 @@ namespace UI
         private IEnumerator WaitAndPrint(float value01_100)
         {
             Debug.Log("Add1");
-            yield return new WaitForSeconds(_delayAddSeconds);
+            yield return new WaitForSeconds(_delayAddSeconds);  // delay 2 seconds for animation
 
             float v01 = Mathf.Clamp01(value01_100 * 0.01f);
             _image.fillAmount = _invert ? (1f - v01) : v01;
