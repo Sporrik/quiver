8c5558c04cb52798a4f357da4fca6dcf27337db0
Sporrik <ahmadyolo@gmail.com>
2025-12-15 15:15:03 +0100

    Squashing main

--- Assets/Scripts/Managers/BarManager.cs
+++ Assets/Scripts/Managers/BarManager.cs
@@ -59,7 +59,6 @@ namespace UI
         #region Unity
         private void Awake()
         {
-
             if (_playerController == null) Debug.LogError($"{nameof(BarManager)}: PlayerController missing.", this);
             if (_scriptableObject == null) Debug.LogError($"{nameof(BarManager)}: UIScriptableObject missing.", this);
 
@@ -70,7 +69,6 @@ namespace UI
 
         private void OnEnable()
         {
-
             if (_playerController != null)
             {
                 _playerController.OnStaminaChanged += HandleStaminaChanged;
