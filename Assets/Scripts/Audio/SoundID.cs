3528818b41f9fd0ee9eac38c8660a335f94fc1e2
Sporrik <ahmadyolo@gmail.com>
2026-01-04 22:31:36 +0100

    audio files pushed

--- Assets/Scripts/Audio/SoundID.cs
+++ Assets/Scripts/Audio/SoundID.cs
@@ -17,6 +17,41 @@ namespace Audio
         Sfx_UIOpen,
         Sfx_UIClose,
         Sfx_UIHover,
-        Sfx_UIClick
+        Sfx_UIClick,
+        
+        //VO
+        VO_Baby_Giggle_01,
+        VO_Baby_Giggle_02,
+        VO_Baby_Giggle_03,
+        VO_Baby_Mad_01,
+        VO_Baby_Mad_02,
+        VO_Baby_Mad_03,
+        VO_Baby_Mad_04,
+        VO_Baby_Sad_01,
+        VO_Baby_Sad_02,
+        VO_Baby_Sad_03,
+        VO_Baby_Sad_04,
+        VO_Baby_Sad_05,
+        VO_Baby_Sad_06,
+        VO_Baby_Sad_07,
+        VO_Baby_Vox_01,
+        VO_Baby_Vox_02,
+        VO_Baby_Vox_03,
+        VO_Baby_Vox_04,
+        VO_Baby_Vox_05,
+        
+        VO_Player_Sprint,
+        VO_Player_Recover,
+        VO_Player_Shush,
+        VO_Player_Hum_01,
+        VO_Player_Hum_02,
+        
+        //Foley
+        Sfx_Footsteps_Sneak,
+        Sfx_Footsteps_Sprint,
+        Sfx_Elevator_Bell,
+        Sfx_Strap,
+        Sfx_Pocket,
+        Sfx_Takedown
     }
 }
